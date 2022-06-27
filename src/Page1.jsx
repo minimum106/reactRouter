545b10488dd5f110fb9aa926f1a205146253f4a2
minimum106 <99700099+minimum106@users.noreply.github.com>
2022-06-27 15:42:00 +0900

    Linkを使用しないページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,15 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { UrlParameter } from "./UrlParameter";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const history = useHistory();
+  const onClickDetailA = () => history.push("/page1/detailA");
   return (
     <div>
       <h1>Page1ページです</h1>
       <Link to={{ pathname: "/page1/detailA", state: arr }}>Page1DetailA</Link>
       <br />
       <Link to="/page1/detailB">Page1DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
